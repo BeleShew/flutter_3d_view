14a6549d52e7b34d38568c3df2b5e3d008ab804e
Bele <Bele555036@gmail.com>
2023-11-20 14:34:09 +0300

    second assesment finished

--- lib/screen/hero_image_details.dart
+++ lib/screen/hero_image_details.dart
@@ -1,61 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_3d_object/screen/home_page.dart';
+import 'package:get/get.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 
+import '../controller/home_controller.dart';
 import 'credit_form.dart';
 
 class HeroProductDetails extends StatelessWidget {
-  HeroProductDetails({super.key,required this.imageUrl});
-  String imageUrl;
+  HeroProductDetails({super.key, required this.imageUrl,required this.heroTag});
+  Widget imageUrl;
+  String heroTag;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Container(
-            height: 250,
-            decoration: const BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.all(Radius.circular(20))
+    return Scaffold(
+      backgroundColor:const Color(0xfffebe9f4),
+      body: SafeArea(
+        child: InkWell(
+          onTap: (){
+            Get.delete<HomeController>(force: true);
+            Get.put(HomeController());
+            // Navigator.of(context).pop();
+            // Navigator.of(context).pop(
+            //   PageRouteBuilder(
+            //     transitionDuration: const Duration(milliseconds: 1000),
+            //     pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+            //       return HomePage(); // Replace with the widget for the replacement page
+            //     },
+            //     transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+            //       return FadeTransition(
+            //         opacity: CurvedAnimation(parent: animation, curve: Curves.elasticInOut),
+            //         child: child,
+            //       );
+            //     },
+            //   ),
+            // );
+          },
+          child: Hero(
+            tag: heroTag,
+            child: Stack(
+              children: [
+                 Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child:imageUrl,
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 200,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                )
+              ],
             ),
-            child:ModelViewer(
-              key: ValueKey(imageUrl),
-              id: "oeuvres3D",
-              backgroundColor: Colors.transparent,
-              src:imageUrl,
-              autoRotate: true,
-              disableZoom: true,
-            )
+          ),
         ),
-        const SizedBox( height: 20,),
-        const Text("Do you want to proceed to checkout?", style:TextStyle(color: Colors.black,fontSize: 14),),
-        const SizedBox( height: 20,),
-
-        Align(
-            alignment: Alignment.bottomRight,
-            child: ElevatedButton(
-                onPressed: (){
-                  Navigator.of(context).pop();
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Align(
-                          alignment:Alignment.center,
-                          child: InkWell(
-                            onTap:(){
-                              Navigator.of(context).pop();
-                            },
-                            child: Text('Credit Form'),
-                          ),
-                        ),
-                        content: CrediForms(),
-                      );
-                    },
-                  );
-                }, child:  const Text("checkout", style:TextStyle(color: Colors.black,fontSize: 12),))
-        ),
-      ],
+      ),
     );
   }
 }
